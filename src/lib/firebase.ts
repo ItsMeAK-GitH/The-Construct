c611817e80fe7cb160a27c1a4c55e0bafdcd8b96
Akshith Rajesh <akshitrajesh2020@gmail.com>
2025-09-07 07:05:23 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuf8fAnFOSTNpCp-YY8wPa9hXL_bnonKM",
-  authDomain: "writenow-qp09o.firebaseapp.com",
-  projectId: "writenow-qp09o",
-  storageBucket: "writenow-qp09o.appspot.com",
-  messagingSenderId: "709911042403",
-  appId: "1:709911042403:web:9b5b4f1217a484dc67db56",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
